afa904664433c58def3059021c8b27f217182d4e
axnir <lxgjzyj@hotmail.com>
2023-04-16 14:02:37 +0800

    chore: configure lint staged

---
+++ site.config.js
@@ -0,0 +1,7 @@
+module.export = {
+  metadata: {
+    title: `Axnir's blog`,
+    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    author: `lxgjzyj@hotmail.com`,
+  },
+};
