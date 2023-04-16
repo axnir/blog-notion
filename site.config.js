7784a0251b1f3c39f85adc87f13c2fb445cda014
axnir <lxgjzyj@hotmail.com>
2023-04-16 14:05:28 +0800

    chore: modify site.config

--- site.config.js
+++ site.config.js
@@ -1,7 +1,7 @@
 module.export = {
   metadata: {
     title: `Axnir's blog`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    description: `Axnir's blog Site`,
     author: `lxgjzyj@hotmail.com`,
   },
 };
