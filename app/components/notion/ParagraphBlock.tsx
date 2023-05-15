e7cd0be5d1484aad635170489ed5c45eda0619cb
axnir <lxgjzyj@hotmail.com>
2023-05-15 23:51:44 +0800

    fix: modify ParagraphBlock component

--- app/components/notion/ParagraphBlock.tsx
+++ app/components/notion/ParagraphBlock.tsx
@@ -10,10 +10,10 @@ export default function ParagraphBlock(
   const { rich_text } = props;
 
   return (
-    <>
+    <p>
       {(rich_text as TextRichTextItemResponse[]).map((text, idx) => (
         <RichText key={idx} isNewLine={rich_text.length === 1} {...text} />
       ))}
-    </>
+    </p>
   );
 }
